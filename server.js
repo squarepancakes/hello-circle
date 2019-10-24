ba4def90a860bb09270978c8e4a6623a3a198e75
squarepancakes <goh.shuyun@gmail.com>
2019-10-24 15:54:26 +0800

    changed message

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
 const app = require("./app")
 const port = process.env.PORT || 3000
 
-app.listen(port, () => console.log(`Hello circle ci listening on port ${port}!`))
+app.listen(port, () => console.log(`Hello circle listening on port ${port}!`))
