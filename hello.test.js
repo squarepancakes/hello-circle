51bfa29e1599bcbb17a20bd4962f320b6df93ec9
squarepancakes <goh.shuyun@gmail.com>
2019-10-24 15:41:15 +0800

    setup simple server

---
+++ hello.test.js
@@ -0,0 +1,10 @@
+const app = require("./app");
+const request = require("supertest")
+
+describe("Server", () => {
+
+    it("GET / should return Hello World", async () => {
+        const response = await request(app).get("/");
+        expect(response.text).toBe("Hello World!")
+    })
+})
